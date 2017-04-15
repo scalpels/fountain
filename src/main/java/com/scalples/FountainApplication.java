378b71e23517408bf1f77924f2a97130e0445ec4
Steven Xu <nonumber1989@gmail.com>
2017-04-15 23:03:18 +0800

    init

---
+++ src/main/java/com/scalples/FountainApplication.java
@@ -0,0 +1,12 @@
+package com.scalples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FountainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FountainApplication.class, args);
+	}
+}
