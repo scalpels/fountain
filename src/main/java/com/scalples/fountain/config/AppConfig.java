1e3e78df96190da6b7f798e285015bfb9d8e7ec6
Steven Xu <nonumber1989@gmail.com>
2017-04-16 18:54:29 +0800

    refator

---
+++ src/main/java/com/scalples/fountain/config/AppConfig.java
@@ -0,0 +1,10 @@
+//package com.scalples.fountain.config;
+//
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@MapperScan("com.scalples.fountain.mapper")
+//public class AppConfig {
+//
+//}
