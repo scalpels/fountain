5831cdbb04cd2e811f484bc25217a65e0cf11340
steven xu <nonumber1989@gmail.com>
2017-05-05 16:50:32 +0800

    sql to postgres style

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
 drop table if exists topic;
 
-create table topic (id bigint  primary key, title varchar(30), description varchar(30),created_on timestamp,last_modified_on timestamp);
+create table topic (id SERIAL  primary key, title varchar(30), description varchar(30),created_on timestamp,last_modified_on timestamp);
 
-insert into topic (id,title, description,created_on,last_modified_on) values (1,'Security Network', '安全网络','2017-04-17','2017-04-17');
-insert into topic (id,title, description,created_on,last_modified_on) values (2,'Java', '一门编程语言','2017-04-17','2017-04-17');
-insert into topic (id,title, description,created_on,last_modified_on) values (3,'SAP', 'ERP软件提供商','2017-04-17','2017-04-17');
+insert into topic (title, description,created_on,last_modified_on) values ('Security Network', '安全网络','2017-04-17','2017-04-17');
+insert into topic (title, description,created_on,last_modified_on) values ('Java', '一门编程语言','2017-04-17','2017-04-17');
+insert into topic (title, description,created_on,last_modified_on) values ('SAP', 'ERP软件提供商','2017-04-17','2017-04-17');
