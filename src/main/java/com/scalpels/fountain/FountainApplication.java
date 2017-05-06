c20d2a4eecbff7b646ce651d475192ec55bd630c
steven xu <nonumber1989@gmail.com>
2017-05-06 17:15:37 +0800

    little change

--- src/main/java/com/scalpels/fountain/FountainApplication.java
+++ src/main/java/com/scalpels/fountain/FountainApplication.java
@@ -3,9 +3,10 @@ package com.scalpels.fountain;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 @Configuration
 @MapperScan("com.scalpels.fountain.mapper")
 public class FountainApplication {
