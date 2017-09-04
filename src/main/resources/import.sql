f6ece19018a29e5f3636f2eac60b2c4eb142d083
nonumber1989 <nonumber1989@gmail.com>
2017-09-04 22:11:40 +0800

    config properties

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,3 @@
-drop table if exists topic;
-
-create table topic (id bigint auto_increment primary key, title varchar(30), description varchar(30),created_on timestamp null,last_modified_on timestamp null);
-
 insert into topic (title, description,created_on,last_modified_on) values ('Security Network', '安全网络','2017-04-17','2017-04-17');
 insert into topic (title, description,created_on,last_modified_on) values ('Java', '一门编程语言','2017-04-17','2017-04-17');
 insert into topic (title, description,created_on,last_modified_on) values ('SAP', 'ERP软件提供商','2017-04-17','2017-04-17');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+drop table if exists topic;
+create table topic (id bigint auto_increment primary key, title varchar(30), description varchar(30),created_on timestamp null,last_modified_on timestamp null);
