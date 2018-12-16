3776c3d418c36bb88bbad50cac58f738d4d92e84
Steven.Xu <nonumber1989@gmail.com>
2018-12-16 12:13:21 +0800

    first

---
+++ src/main/java/com/scalpels/fountain/mapper/ConfigurationMapper.java
@@ -0,0 +1,30 @@
+package com.scalpels.fountain.mapper;
+
+import com.scalpels.fountain.model.Configuration;
+import com.scalpels.fountain.model.ConfigurationExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ConfigurationMapper {
+    long countByExample(ConfigurationExample example);
+
+    int deleteByExample(ConfigurationExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Configuration record);
+
+    int insertSelective(Configuration record);
+
+    List<Configuration> selectByExample(ConfigurationExample example);
+
+    Configuration selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Configuration record, @Param("example") ConfigurationExample example);
+
+    int updateByExample(@Param("record") Configuration record, @Param("example") ConfigurationExample example);
+
+    int updateByPrimaryKeySelective(Configuration record);
+
+    int updateByPrimaryKey(Configuration record);
+}
---
+++ src/main/java/com/scalpels/fountain/model/Configuration.java
@@ -0,0 +1,75 @@
+package com.scalpels.fountain.model;
+
+import java.util.Date;
+
+public class Configuration {
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String createdBy;
+
+    private Date createdOn;
+
+    private String lastModifiedBy;
+
+    private Date lastModifiedOn;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy == null ? null : createdBy.trim();
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy == null ? null : lastModifiedBy.trim();
+    }
+
+    public Date getLastModifiedOn() {
+        return lastModifiedOn;
+    }
+
+    public void setLastModifiedOn(Date lastModifiedOn) {
+        this.lastModifiedOn = lastModifiedOn;
+    }
+}
