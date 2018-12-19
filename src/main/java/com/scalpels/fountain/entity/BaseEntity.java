d8ea30abbb9417b931cb3b586917850602a81334
Steven.Xu <nonumber1989@gmail.com>
2018-12-19 22:06:51 +0800

    root

---
+++ src/main/java/com/scalpels/fountain/entity/BaseEntity.java
@@ -0,0 +1,55 @@
+package com.scalpels.fountain.entity;
+
+import java.util.Date;
+
+public class BaseEntity {
+    private Long id;
+
+    private String createdBy;
+
+    private Date createdOn;
+
+    private String lastModifiedBy;
+
+    private Date lastModifiedOn;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public Date getLastModifiedOn() {
+        return lastModifiedOn;
+    }
+
+    public void setLastModifiedOn(Date lastModifiedOn) {
+        this.lastModifiedOn = lastModifiedOn;
+    }
+}
